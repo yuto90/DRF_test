41c9b513e85239452b4bda0fdffd55c44e0967c1
yuto <flyingfox4087@yahoo.co.jp>
2021-06-06 12:26:40 +0900

    SECRET_KEY変更

--- drf/config/settings.py
+++ drf/config/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6m8yxtm$av%3n9rlv))qhdi0s(0l@(-w^15hji3o1=og56qpv('
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
